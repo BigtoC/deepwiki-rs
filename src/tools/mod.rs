e6ad10dda54718915b6fae2730d4e60d55b63273
sopaco <dokhell@hotmail.com>
2025-09-08 18:55:05 +0800

    Remove architecture detector and code analyzer tools
    Remove file explorer tool
    
    Update tools module to reflect removed tools

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,6 +1,3 @@
-pub(crate) mod file_explorer;
-pub(crate) mod code_analyzer;
 pub(crate) mod dependency_analyzer;
-pub(crate) mod architecture_detector;
 
 pub(crate) use dependency_analyzer::DependencyAnalyzerTool;
